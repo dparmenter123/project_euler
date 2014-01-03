8c441ae2a6ad84afe2562ae02f98679adc542a9b
David Parmenter <dparment@adobe.com>
2014-01-03 08:11:06 -0500

    cleaned up the utils tests a bit. added ability to start gen_primes() at an arbitrary place.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,28 @@
 import sys
 import unittest
-from utils import rwh_primes
+from utils import rwh_primes, gen_primes, is_prime
 
-class problem1(unittest.TestCase):
-    # test rwh primes
+class Test(unittest.TestCase):
     def test_rwh_primes(self):
         primes = rwh_primes(10)
         self.assertEqual([2, 3, 5, 7], primes)
         primes = rwh_primes(20)
         self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19], primes)
 
+    def test_gen_primes(self):
+        gp = gen_primes()
+        self.assertEqual([2,3,5,7,11], [gp.next() for i in range(5)])
+
+    def test_gen_primes_start(self):
+        gp = gen_primes(6)
+        self.assertEqual([7,11,13,17,19], [gp.next() for i in range(5)])
+
+    def test_is_prime(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime(109))
+        self.assertTrue(is_prime(673))
+        self.assertFalse(is_prime(1))
+        self.assertFalse(is_prime(15))
+        self.assertFalse(is_prime(999))
--- utils.py
+++ utils.py
@@ -5,14 +5,12 @@ import logging
 # logging
 #
 # TODO: move logging configuration into a separate file
-################################################################
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 ################################################################
 # timer
-################################################################
 
 def timeit(method):
     '''
@@ -37,7 +35,6 @@ def timeit(method):
 ################################################################
 # rwh_primes
 # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
-################################################################
 
 def rwh_primes(n):
     """ Returns  a list of primes < n """
@@ -47,6 +44,7 @@ def rwh_primes(n):
             sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
     return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
+################################################################
 def numberize(digits):
     '''
      Take a sequence and multiply it back together to form a number
@@ -56,6 +54,7 @@ def numberize(digits):
         result = result * 10 + digit
     return result
 
+################################################################
 def digitize(N):
     '''
      Take the given number and return it as a list of digits
@@ -70,11 +69,12 @@ def digitize(N):
     return result
         
 
+################################################################
 # Sieve of Eratosthenes
 # Code by David Eppstein, UC Irvine, 28 Feb 2002
 # http://code.activestate.com/recipes/117119/
 
-def gen_primes():
+def gen_primes(start=2):
     """ Generate an infinite sequence of prime numbers.
     """
     # Maps composites to primes witnessing their compositeness.
@@ -93,7 +93,8 @@ def gen_primes():
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
             # 
-            yield q        
+            if q >= start:
+                yield q        
             D[q * q] = [q]
         else:
             # q is composite. D[q] is the list of primes that
@@ -108,16 +109,20 @@ def gen_primes():
 
         q += 1
 
+################################################################
 
 def is_prime(n):
-  if n == 2 or n == 3: return True
-  if n < 2 or n%2 == 0: return False
-  if n < 9: return True
-  if n%3 == 0: return False
-  r = int(n**0.5)
-  f = 5
-  while f <= r:
-    if n%f == 0: return False
-    if n%(f+2) == 0: return False
-    f +=6
-  return True
+    ''' fast test for primality'''
+
+    if n == 2 or n == 3: return True
+    if n < 2 or n%2 == 0: return False
+    if n < 9: return True
+    if n%3 == 0: return False
+
+    r = int(n**0.5)
+    f = 5
+    while f <= r:
+        if n%f == 0: return False
+        if n%(f+2) == 0: return False
+        f +=6
+    return True
